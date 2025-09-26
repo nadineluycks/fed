d2fd3fdda4539b4bfcf0279a87db3b6b12bff6d6
nadineluycks <nadineluycks@gmail.com>
2025-09-26 11:30:21 +0200

    eerste pagina af

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,28 @@
 // JavaScript Document
 console.log("hi");
+// selecteer de knoppen
+// const openButton = document.querySelector("header nav button"); 
+// const sluitButton = document.querySelector("header nav button");  
+// const deNav = document.querySelector("header nav");                
+
+// // klik op hamburger → schuif menu naar rechts
+// openButton.addEventListener("click", () => {
+//   deNav.classList.add('open');
+// });
+
+// // klik op kruisje → schuif menu terug naar links
+// sluitButton.addEventListener("click", () => {
+//   deNav.classList.remove('open');
+// });
+
+const openButton = document.querySelector(".open-menu"); 
+const sluitButton = document.querySelector(".sluit-menu");  
+const deNav = document.querySelector("header nav");                
+
+openButton.addEventListener("click", () => {
+  deNav.classList.add("open");
+});
+
+sluitButton.addEventListener("click", () => {
+  deNav.classList.remove("open");
+});
